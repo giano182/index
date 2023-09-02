5f0905ab2c839ef1514958a5b660f8ea59d3d3f3
giano182 <amer.elghali2006@gmail.com>
2023-09-02 13:16:06 +0100

    last

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () » {
+    sections.forEach(sec {
+    let top = window.scrollY
+    let offset =sec.offset
+    }
+        )
+
+}
